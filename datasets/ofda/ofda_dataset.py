3028017f896576d855bf18d7a29c1d898248283f
Alain <alain@dealain.com>
2023-04-18 16:11:15 -0500

    show images

--- datasets/ofda/ofda_dataset.py
+++ datasets/ofda/ofda_dataset.py
@@ -3,6 +3,8 @@ import cv2
 import h5py
 import os
 import glob
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class OFDASegmented:
@@ -25,6 +27,8 @@ class OFDASegmented:
             if not self.save_distance_transform(filename, os.path.join(dst_dir, os.path.basename(filename).replace('.png','.h5')), as_h5):
                 return False
         return True
+    
+    #def show_distance_maps(self):
         
 
 class TestOFDASegmented(unittest.TestCase):
@@ -34,7 +38,19 @@ class TestOFDASegmented(unittest.TestCase):
         dataset = OFDASegmented(src_dir)
         #dataset.distance_transform()
         self.assertEqual(dataset.export_as_distance_maps(dst_dir), True, "exportación satisfactoria")
+
+    def test_show_distance_maps(self):
+        dir = "gt_dm/"
+        i = 0
+        for filename in glob.glob(dir + "*.h5"):
+            mask_h5 = h5py.File(filename, 'r')
+            mask = np.asarray(mask_h5['dm'])
+            plt.subplot(1, 5, i+1)
+            plt.imshow(mask, cmap='gray')
+            i+=1
         
+        #plt.imshow(mask, cmap="gray")
+        plt.show()
         
 if __name__ == "__main__":
     unittest.main()
